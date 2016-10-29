96002c19b5383e746be68fb31d465fea7c5dd221
sumanmaity112 <suman.maity112@gmail.com>
2016-10-29 16:43:41 +0530

    Suman | Create vertical bar for students score which have ability to sort dom by name, score and subject

---
+++ scripts/studentScores.js
@@ -0,0 +1,75 @@
+var data = [
+    {name: 'ramesh', subject: 'maths', score: 87},
+    {name: 'suresh', subject: 'maths', score: 45},
+    {name: 'pokemon', subject: 'english', score: 65},
+    {name: 'mary', subject: 'kannada', score: 44},
+    {name: 'riya', subject: 'science', score: 72},
+    {name: 'katie', subject: 'social studies', score: 82},
+    {name: 'katie', subject: 'maths', score: 98},
+    {name: 'ramesh', subject: 'bengali', score: 25},
+    {name: 'suresh', subject: 'science', score: 55},
+    {name: 'riya', subject: 'tamil', score: 75},
+    {name: 'pokemon', subject: 'sports', score: 95},
+    {name: 'pokemon', subject: 'social studies', score: 32}
+];
+
+var uniqueSubjects = [];
+
+var color = d3.scaleOrdinal(d3.schemeCategory10)
+    .domain(uniqueSubjects);
+
+var widthScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([0, 600]);
+
+var createChart = function (container, data) {
+    container.selectAll(".bars").data(data).enter()
+        .append("div")
+        .classed("bars", true)
+        .classed("radial-corner", true)
+        .text(function (d) {
+            return d.name + " " + d.score;
+        })
+        .style("width", function (d) {
+            return widthScale(d.score) + "px";
+        })
+        .style("background-color", "black")
+        .style("background-color", function (d) {
+            return color(d.subject)
+        });
+};
+
+var sortBy = function (fieldName) {
+    var bars = d3.selectAll(".bars");
+    bars.sort(function (recent, previous) {
+        if (recent[fieldName] < previous[fieldName])
+            return -1;
+        if (recent[fieldName] > previous[fieldName])
+            return 1;
+        return recent.score - previous.score;
+    });
+};
+
+
+var getUniqueSubjects = function (data) {
+    data.forEach(function (d) {
+        if (uniqueSubjects.indexOf(d.subject) == -1)
+            uniqueSubjects.push(d.subject);
+    });
+    return uniqueSubjects;
+};
+
+var createSubjectsButton = function (data) {
+    var subjects = getUniqueSubjects(data);
+    var innerHtml = "";
+    subjects.forEach(function (subject) {
+        innerHtml += "<div style='background-color: " + color(subject) + "' class='legend floating-block'>" + subject + "</div>";
+    });
+    document.getElementById("subject-legend").innerHTML += innerHtml;
+};
+
+window.onload = function () {
+    createSubjectsButton(data);
+    var container = d3.select("#container");
+    createChart(container, data);
+};
