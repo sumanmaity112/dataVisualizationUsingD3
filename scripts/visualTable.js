76ebfd4e8250c30013a72edb8d4981e0e5d3d70c
sumanmaity112 <suman.maity112@gmail.com>
2016-11-07 16:07:44 +0530

    Suman | Refactored visual table code

--- scripts/visualTable.js
+++ scripts/visualTable.js
@@ -1,11 +1,22 @@
 var LOG = d3.scaleLog().domain([1, 10]);
+var ROUNDED_LOG = d3.scaleLog().domain([1, 10]).rangeRound([0, 1]);
 var FORMAT = d3.format(".4f");
 var POW = d3.scalePow().exponent(2);
+var getLog = function (value) {
+    return FORMAT(LOG(value));
+};
+
+var generateSameValue = function (value) {
+    return value;
+};
+var generators = [generateSameValue, generateSameValue, POW, getLog, ROUNDED_LOG];
+var columnHeaders = ["Title", "n", "n^2", "log(n)", "rounded log(n)"];
+var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 var insertData = function (visualTable, data, generator) {
     visualTable.append("div")
         .classed("row", true)
-        .selectAll("cell")
+        .selectAll(".cell")
         .data(data)
         .enter()
         .append("div")
@@ -15,30 +26,13 @@ var insertData = function (visualTable, data, generator) {
         });
 };
 
-var getLog = function (value) {
-    return FORMAT(LOG(value));
+var main = function (data, generators, columnHeaders) {
+    return function () {
+        var visualTable = d3.select("#container").append("div").attr("id", "visualTable");
+        generators.forEach(function (generator, index) {
+            insertData(visualTable, new Array(columnHeaders[index]).concat(data), generator);
+        });
+    }
 };
 
-var getRoundedLog = function (value) {
-    return Math.round(LOG(value));
-};
-
-var createTable = function (visualTable, data, generators, columnHeaders) {
-    generators.forEach(function (generator, index) {
-        insertData(visualTable, new Array(columnHeaders[index]).concat(data), generator);
-    });
-};
-
-var main = function () {
-    var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    var visualTable = d3.select("#container").append("div").attr("id", "visualTable");
-
-    var generateSameValue = function (value) {
-        return value;
-    };
-    var generators = [generateSameValue, generateSameValue, POW, getLog, getRoundedLog];
-    var columnHeaders = ["Title", "n", "n^2", "log(n)", "rounded log(n)"];
-    createTable(visualTable, data, generators, columnHeaders);
-};
-
-window.onload = main;
+window.onload = main(data, generators, columnHeaders);
