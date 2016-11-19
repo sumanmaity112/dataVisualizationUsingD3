e1ddde43e1dc8ea002ec4c79be83861c18e2896d
sumanmaity112 <suman.maity112@gmail.com>
2016-11-19 16:56:05 +0530

    Suman | Added ability to set dynamic interpolator for area chart

--- scripts/areaChart.js
+++ scripts/areaChart.js
@@ -3,6 +3,17 @@ const HEIGHT = 600;
 const MARGIN = 30;
 const INNER_WIDTH = WIDTH - 2 * MARGIN;
 const INNER_HEIGHT = HEIGHT - 2 * MARGIN;
+var SVG;
+
+var curves = [
+    {name: "Linear Curve", value: d3.curveLinear},
+    {name: "Closed Linear Curve", value: d3.curveLinearClosed},
+    {name: "Step After Curve", value: d3.curveStepAfter},
+    {name: "Basis Curve", value: d3.curveBasis},
+    {name: "Cardinal Closed Curve", value: d3.curveCardinalClosed},
+    {name: "Cardinal Curve", value: d3.curveCardinalOpen}
+];
+
 
 var valueGenerator = function (value) {
     return {x: value / 10, y: ((3 * Math.sin(value)) + 5) / 10}
@@ -11,7 +22,7 @@ var valueGenerator = function (value) {
 var XScale = getScale([0, 1.0], [0, INNER_WIDTH]);
 var YScale = getScale([0, 1.0], [INNER_HEIGHT, 0]);
 
-var area = d3.area()
+var AREA = d3.area()
     .x(function (d) {
         return XScale(d.x);
     })
@@ -19,20 +30,30 @@ var area = d3.area()
     .y1(function (d) {
         return YScale(d.y);
     });
+var LINE = getLine(XScale, YScale, "x", "y");
 
+var drawArea = function () {
+    var curve = getSelectedValue(d3.select("select"), curves);
+    clearSVG();
+    drawLine(SVG, AREA.curve(curve), VALUES, "area", translate(MARGIN, MARGIN));
+    drawLine(SVG, LINE.curve(curve), VALUES, "line", translate(MARGIN, MARGIN));
+    drawCircles(SVG, VALUES, 4.5);
+};
+
+var clearSVG = function () {
+  d3.selectAll(".area").remove();
+  d3.selectAll(".line").remove();
+  d3.selectAll("circle").remove();
+};
 
 var generateChart = function () {
-    var values = generateValues(valueGenerator, 0, 10);
-    var svg = d3.select("#container").append("svg")
+    appendOptions(d3.select("#container"), curves, drawArea);
+    VALUES = generateValues(valueGenerator, 0, 10);
+    SVG = d3.select("#container").append("svg")
         .attr("height", HEIGHT)
         .attr("width", WIDTH);
-    var line = getLine(XScale, YScale, "x", "y");
-    drawAxis(svg, d3.axisLeft(YScale), translate(MARGIN, MARGIN));
-    drawAxis(svg, d3.axisBottom(XScale), translate(MARGIN, HEIGHT - MARGIN));
-
-    drawLine(svg, line, values, "line", translate(MARGIN, MARGIN));
-    drawLine(svg, area, values, "area", translate(MARGIN + 1, MARGIN));
-    drawCircles(svg, values, 4.5);
+    drawAxis(SVG, d3.axisLeft(YScale), translate(MARGIN, MARGIN));
+    drawAxis(SVG, d3.axisBottom(XScale), translate(MARGIN, HEIGHT - MARGIN));
 
 };
 
