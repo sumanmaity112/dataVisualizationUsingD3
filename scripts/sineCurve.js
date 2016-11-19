cd8e527798a1dedce8b02f1b63ad45a24dc5cc5f
sumanmaity112 <suman.maity112@gmail.com>
2016-11-19 14:20:19 +0530

    Suman | Added ability to set dynamic tension

--- scripts/sineCurve.js
+++ scripts/sineCurve.js
@@ -4,6 +4,44 @@ const MARGIN = 30;
 const INNER_WIDTH = WIDTH - 2 * MARGIN;
 const INNER_HEIGHT = HEIGHT - 2 * MARGIN;
 
+var curves = [
+    {name: "0", value: 0},
+    {name: "20", value: 20},
+    {name: "40", value: 40},
+    {name: "60", value: 60},
+    {name: "80", value: 80},
+    {name: "100", value: 100}
+];
+
+var drawCurves = function (svg) {
+    var values = generateValuePoints();
+    return function (tensionInPercentage) {
+        d3.select(".line").remove();
+        tensionInPercentage = tensionInPercentage || getTension();
+        line = line.curve(d3.curveCardinal.tension(tensionScale(tensionInPercentage)));
+        drawLine(svg, line, values, "line", true);
+    }
+
+};
+
+var appendOptions = function (svg) {
+    var optionContainer = d3.select("#container").append("div").classed("optionContainer", true);
+    optionContainer.text('Tension').append("select").on("change", drawCurves(svg))
+        .selectAll("option")
+        .data(curves)
+        .enter()
+        .append("option")
+        .text(function (curve) {
+            return curve.name;
+        });
+};
+
+var getTension = function () {
+    var select = d3.select('select');
+    var selectedIndex = select.property('selectedIndex');
+    return curves[selectedIndex].value;
+};
+
 var XScale = d3.scaleLinear()
     .domain([0, 10])
     .range([0, INNER_WIDTH]);
@@ -65,23 +103,27 @@ var generateValuePoints = function () {
     }
     return sineValues
 };
+var line = d3.line()
+    .x(function (d) {
+        return XScale(d.x);
+    })
+    .y(function (d) {
+        return YScale(d.y);
+    });
+
+
+var tensionScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([-1, 1]);
 
 var generateChart = function () {
-    var line = d3.line()
-        .x(function (d) {
-            return XScale(d.x);
-        })
-        .y(function (d) {
-            return YScale(d.y);
-        });
-
-    var values = generateValuePoints();
     var svg = d3.select("#container").append("svg")
         .attr("height", HEIGHT)
         .attr("width", WIDTH);
     drawAxis(svg, XAxis, translate(MARGIN, HEIGHT - MARGIN));
     drawAxis(svg, YAxis, translate(MARGIN, MARGIN));
-    drawLine(svg, line, values, "line", true);
+    appendOptions(svg);
+    drawCurves(svg)(0);
 };
 
 window.onload = generateChart;
